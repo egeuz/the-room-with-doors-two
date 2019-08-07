f1b10b3a1bd42c21d39992e1592fa0365a19f5d0
Ege Uz <enkomaun@gmail.com>
2019-08-07 10:16:52 -0400

    re-organized room2 structure

---
+++ js/room1.js
@@ -0,0 +1,36 @@
+//RNG function
+function rng(max, min) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//Select the correct door at random
+const correctDoorRNG = rng(1, 9);
+const correctDoor = document.querySelector(`.door-${correctDoorRNG}`);
+correctDoor.classList.add('correct-door');
+correctDoor.href = "room2.html";
+
+//Taunting text array
+const taunts = [
+  "Whoopsie.",
+  "That's a slippery one.",
+  "You'll get it next time!",
+  "Ah, almost.",
+  "Believe in yourself!",
+  "Maybe try another one?",
+  "Ack!",
+  "Oh no!",
+  "Actually try this time."
+];
+
+const text = document.querySelector('#text-box p');
+const doors = document.querySelectorAll('.door');
+
+//Change textbox on hover
+[...doors].forEach(function(door) {
+  door.addEventListener('mouseenter', function(){
+    let tauntRNG = rng(0, taunts.length);
+    text.innerHTML = taunts[tauntRNG];
+  });
+});
+
+
---
+++ js/room2.js
@@ -0,0 +1,59 @@
+//Initialization
+const shapes = ["circle", "circle", "square", "square", "triangle", "triangle", "pentagon", "pentagon", "star", "star"];
+shuffle(shapes);
+
+//Shuffle algorithm
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+    [array[i], array[j]] = [array[j], array[i]]; // swap elements
+  }
+}
+
+//Distribute shapes randomly behind doors
+const backgroundImages = document.querySelectorAll('.matching-card a');
+backgroundImages.forEach((image) => {
+  image.style.backgroundImage = `url(../assets/${shapes[0]}.png)`;
+  image.classList.add(shapes[0]);
+  shapes.shift();
+});
+
+//Get the correct shape, add the next room link to it
+const correctShape = shapes[0];
+const correctLink = document.querySelector(`.${shapes[0]}`);
+correctLink.href = 'room3.html'
+
+const doors = document.querySelectorAll('.door');
+let openedDoors = [];
+
+doors.forEach((door) => {
+  door.addEventListener('click', (event) => {
+    event.target.classList.add('open');
+    openedDoors.push(event.target);
+    if(openedDoors.length === 2) {
+      const firstShape = openedDoors[0].parentNode.firstChild.classList;
+      const secondShape = openedDoors[1].parentNode.firstChild.classList;
+      console.log(firstShape);
+    }
+  });
+});
+
+
+
+// //Handling interactions
+// const doors = document.querySelectorAll('.door');
+// let openedDoors = [];
+// doors.forEach((door) => {
+//   door.addEventListener('click', (event) => {
+//     event.target.classList.add('open'); //Open door animation
+//     openedDoors.push(event.target); //Track doors that are open;
+//     console.log(openedDoors);
+
+//     if(openedDoors.length === 2) {
+//       const door1Shape = openedDoors[0].children[0].classList;
+//       console.log(door1Shape);
+//     }
+//   });
+// });
+
+//Checking whether doors match
